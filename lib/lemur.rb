c3d64d7a82cdf324ccab2ee9387362d758c7ca9c
Bob McWhirter <bob@fnokd.com>
2008-08-24 20:40:11 -0400

    First jaunt of pushing in patching base instance method patching from a module

--- lib/lemur.rb
+++ lib/lemur.rb
@@ -1,7 +1,62 @@
 
-class Lemur
+module Lemur
 
-  def self.patch
+  def self.patch_records(patched_class)
+    @patch_records ||= {}
+    records = @patch_records[ patched_class ]
+    unless ( records )
+      records = []
+      @patch_records[ patched_class ] = records
+    end
+    records
+  end
+  
+  def self.check_previous_application( original_class, patch_module )
+    records = patch_records( original_class )
+    return unless records
+    records.each do |r|
+      if ( r.patch_module == patch_module )
+        raise PatchAppliedException.new
+      end
+    end
   end
 
+  def self.patch_class(original_class, patch_module)
+    check_previous_application( original_class, patch_module )
+    patch_method_names = patch_module.public_instance_methods(false).collect{|e|e.to_s}
+    
+    original_class.public_instance_methods.each do |m|
+      if ( patch_method_names.include?( m.to_s ) )
+        Lemur.patch_records( original_class ) << PatchRecord.new( original_class, 
+                                                                  m,
+                                                                  original_class.instance_method( m ),
+                                                                  patch_module,
+                                                                  patch_module.instance_method( m ) )
+        original_class.class_eval( "alias_method :lemur_#{m}, :#{m}" )
+        original_class.class_eval( "remove_method :#{m}" )
+      end
+    end
+
+    original_class.class_eval( "include #{patch_module}" )
+  end
+
+  class PatchRecord
+
+    attr_accessor :patched_class
+    attr_accessor :method_name
+    attr_accessor :original_method
+    attr_accessor :patch_module
+    attr_accessor :patch_method
+
+    def initialize(patched_class, method_name, original_method, patch_module, patch_method)
+      @patched_class   = patched_class
+      @method_name     = method_name
+      @original_method = original_method
+      @patch_module    = patch_module
+      @patch_method    = patch_method
+    end
+  end
+
+  class PatchAppliedException < Exception
+  end
 end
--- spec/lemur_spec.rb
+++ spec/lemur_spec.rb
@@ -1,15 +1,64 @@
 
 require 'lemur'
 
+module PatchModule
+  def some_instance_method()
+    @value = "#{self.class.name} + PatchModule"
+  end
+
+  def some_other_instance_method()
+    @value = "#{self.class.name} + PatchModule + other"
+  end
+end
+
+class BasicClass
+
+  def value
+    @value
+  end
+
+  def some_instance_method()
+    @value = "#{self.class.name} + BasicClass"
+  end
+
+end
+
+
 describe Lemur do
 
-  describe "can patch globally" do
+  before(:all) do 
+    Lemur.patch_class( BasicClass, PatchModule )
   end
 
-  describe "can patch a class" do
+  it "should mix in module methods" do
+    o = BasicClass.new
+    o.some_other_instance_method
+    o.value.should eql( "BasicClass + PatchModule + other" )
   end
 
-  describe "can patch a module" do
+  it "should override instance methods" do
+    o = BasicClass.new
+    o.some_instance_method
+    o.value.should eql( "BasicClass + PatchModule" )
+  end
+
+  it "should track patch records by class" do
+    records = Lemur.patch_records(BasicClass)
+    records.should_not be_nil
+    records.should_not be_empty
+    records.size.should eql( 1 )
+    records[0].patched_class.should be(BasicClass)
+    records[0].method_name.should eql( "some_instance_method" )
+    records[0].original_method.should_not be_nil
+    records[0].patch_module.should be( PatchModule )
+    records[0].patch_method.should_not be_nil
+  end
+
+  it "should prevent duplicate application of the same patch module" do
+      #Lemur.patch_class( BasicClass, PatchModule )
+    lambda {
+      Lemur.patch_class( BasicClass, PatchModule )
+    }.should raise_error( Lemur::PatchAppliedException )
   end
 
 end
